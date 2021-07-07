680c5acae700ad33804be4ce3f22104a7b769b81
AliaksandrBeliayeuStepik <aliaksandrbeliayeu@gmail.com>
2021-07-08 00:49:59 +0300

    Added new BasePage - Product Page, added new locators for it, added new tests for it, added parametrization for different URLs for this page

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,10 @@ class LoginPageLocators():
     REGISTRATION_PW1 = (By.CSS_SELECTOR, "[name = 'registration-password1']")
     REGISTRATION_PW2 = (By.CSS_SELECTOR, "[name = 'registration-password2']")
     REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "[name = 'registration_submit']")
+
+class ProductsPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    MAIN_PRODUCT_BOOK = (By.CSS_SELECTOR, ".col-sm-6.product_main>:nth-child(1)")
+    MAIN_PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main>.price_color")
+    ALERT_PRODUCT_ADDED = (By.CSS_SELECTOR, "#messages>:nth-child(1)>:nth-child(2)>strong")
+    ALERT_CURRENT_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
+import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.test_product_page import ProductsPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -18,3 +20,19 @@ def test_guest_should_see_login_and_registration_forms(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_put_product_to_basket(browser, link):
+    page = ProductsPage(browser, link)
+    page.open()
+    page.adding_product_to_cart()
+
